64824bab4cbd324bd1f52211384259e23e4ae15c
maksvv-m <maksvv.m@yandex.by>
2022-06-01 15:47:29 +0300

    Initial commit

---
+++ src/main/java/by/maksimovich/currency/CurrencyApplication.java
@@ -0,0 +1,14 @@
+package by.maksimovich.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Enable
+@SpringBootApplication
+public class CurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyApplication.class, args);
+	}
+
+}
